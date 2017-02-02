db483eea567071b743027f9374f3a0fdcd8a518f
bindupothana <himabindu.pothana@navaratan.com>
2017-02-02 10:28:11 +0530

    Initial commit

--- js/controllers/employees.js
+++ js/controllers/employees.js
@@ -1,4 +1,20 @@
-var app=angular.module('employeeDemoApp');
-app.controller('employeectrl',function($scope,$state,Employeeservice,$stateparams){
-	
-})
+	var app=angular.module('employeeDemoApp');
+	app.controller('employeectrl',function($scope,$state,$stateparams,EmployeeService){
+		console.log("Registrtion")
+		   
+
+		    $scope.saveEmployee = function(formname){
+		 	console.log($scope.employee);
+		 	EmployeeService.saveEmployee($scope.employee);
+		 	$state.go("employees");
+
+		 }
+		 $scope.employeeslist=Employeeservice.getEmployees();
+		 if($stateparams && $stateparams.id){
+	 	    $scope.employee =EmployeeService.getEmployees($stateparams.id);
+		 }
+		 $scope.updateEmployee = function(req){
+		 	EmployeeService.updateEmployee($stateparams.id,$scope.employee);
+		 	$state.go("employees")
+		 }
+	});
