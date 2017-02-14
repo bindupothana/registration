dd6285d46fbe422d05808e55cb846caef00c4bce
bindupothana <himabindu.pothana@navaratan.com>
2017-02-14 12:04:14 +0530

    Employee details

--- js/services/employee.js
+++ js/services/employee.js
@@ -1,41 +1,50 @@
 var app = angular.module('employeeDemoApp');
 app.factory('EmployeeService',function() {
 
-    var factory = {};
-    var employeeList = []; 
-            
-    factory.saveEmployee = function(employee){
-        employeeList =  JSON.parse(localStorage.getItem('employeeList')); 
-       return employeeList.push(employee);
-      
-      localStorage.setItem('employeeList', JSON.stringify(employeeList));
-       }
-    
-       factory.getEmployees = function(){
-   
-       var employeeList =  JSON.parse(localStorage.getItem('employeeList'));
-       console.log(JSON.parse(localStorage.getItem('employeeList')));
-       return employeeList;
-       }
+  var factory = {};
+  var employeeList=[];
 
 
-    factory.getEmployee = function(index){
-       
-       console.log(employeeList, employeeList[index])
-      return angular.copy(employeeList[index]);
-    }
-    
+  factory.saveEmployee = function(employee) {
 
-    factory.updateEmployee = function(index, employee){
-      
-      return employeeList[index] = employee;
-    }
 
-    factory.deleteEmployee = function(index){
-        return employeeList.splice(index, 1);
-    }
+    var employeeList = JSON.parse(localStorage.getItem("employeesList"));  
+    employeeList.push(employee);
+    localStorage.setItem("employees", JSON.stringify(employeeList));
+  }
 
-    return factory;
 
+
+
+  factory.getEmployees = function(){
+    var employeeList = JSON.parse(localStorage.getItem("employees"));  
+    return employeeList;
+  }
+  factory.getEmployee = function(index){
+
+   console.log(employeeList, employeeList[index])
+   return angular.copy(employeeList[index]);
+ }
+ factory.updateEmployee = function(index, employee){
+
+
+
+   if (localStorage && EmployeesArr) {  
+
+    localStorage.setItems("employees", angular.toJson(EmployeesArr));  
+
+
+
+    return employeeList[index] = employee;
+  }
+  factory.deleteEmployee = function(index){
+
+    return employeeList.splice(index,1);
+  }
+
+
+  return factory;
+
+}
 
 });
